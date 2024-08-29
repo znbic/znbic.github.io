c32a46e1301948b83d8f22f16c604dbc0b3e499a
znbic <znbic@foxmail.com>
2024-08-29 17:31:33 +0800

    fix relative: 4th try

--- assets/js/auto-relative-url.js
+++ assets/js/auto-relative-url.js
@@ -1,9 +1,10 @@
+var baseUrl = "{{ site.baseurl | default: '' }}";
 window.onload = function() {
     var imgs = document.getElementsByTagName("img");
     for (var i = 0; i < imgs.length; i++) {
         var src = imgs[i].getAttribute("src");
         if (src.startsWith("../")) {
-            imgs[i].setAttribute("src", "{{'" + src + "' | relative_url }}");
+            imgs[i].setAttribute("src", baseUrl + src.substring(2));
         }
     }
 };
