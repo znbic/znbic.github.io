6899e2042b4d889b9d636cad27f790e6fa2a78cb
znbic <znbic@foxmail.com>
2024-08-29 12:58:43 +0800

    change relative path problem

---
+++ assets/js/auto-relative-url.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    var imgs = document.getElementsByTagName("img");
+    for (var i = 0; i < imgs.length; i++) {
+        var src = imgs[i].getAttribute("src");
+        if (src.startsWith("../")) {
+            imgs[i].setAttribute("src", "{{'" + src + "' | relative_url }}");
+        }
+    }
+};
